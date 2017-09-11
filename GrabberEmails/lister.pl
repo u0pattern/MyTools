c17384c5a3729a77d075c827e3a7035eb613894a
1337r00t <u9cc85@gmail.com>
2017-09-11 04:42:57 +0300

    Update lister.pl

--- GrabberEmails/lister.pl
+++ GrabberEmails/lister.pl
@@ -18,13 +18,13 @@ while (<MYFILE>) {
     chomp;
     my @emails = split(' ');
     foreach my $email (@emails) {  
-        if(Email::Valid->address($email)) {
-            $getted = "$email\n";
+		if(Email::Valid->address($email)) {
+			$getted = "$email\n";
 			print "$getted\n";
 			open(EM4IL,">>Emails.txt");
 			print EM4IL $getted;
 			close(EM4IL);
-        }
+		}
 		else
 		{
 			print "Sorry, we're not found any email";
